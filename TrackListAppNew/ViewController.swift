aaa1a81462d024e8f5a87f4e77255261e1e87ad9
Sergey Kosichkin <d978827@icloud.com>
2022-12-15 07:27:55 +0300

    Initial Commit

---
+++ TrackListAppNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackListAppNew
+//
+//  Created by Sergey Kosichkin on 15.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
